f97b3e07632bac2da560c4327d4a360e8f6050bc
SirPakun <mafburgmufbarg@gmail.com>
2025-04-14 02:11:21 +0300

    Update

--- src/main/java/by/kirienko/TaxiParkApplication.java
+++ src/main/java/by/kirienko/TaxiParkApplication.java
@@ -1,15 +1,12 @@
 package by.kirienko;
 
 import by.kirienko.view.ViewRegistry;
-
-/**
- * Hello world!
- */
 public class TaxiParkApplication {
 
     public static void main(String[] args) {
         ViewRegistry.MAIN_MENU_VIEW.getView().handleView();
-
+        /*
+        * */
 
     }
 }
